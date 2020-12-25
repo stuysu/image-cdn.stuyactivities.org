7d8cc03feb547a98085b25dc59ae3f014cdece7a
Abir Taheer <abir488@gmail.com>
2020-12-25 20:48:29 +0000

    fixed types of env variables

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const minio = require("minio");
 
 const client = new minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
-  port: process.env.MINIO_PORT,
-  useSSL: process.env.MINIO_USE_SSL,
+  port: parseInt(process.env.MINIO_PORT),
+  useSSL: process.env.MINIO_USE_SSL === "true",
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
